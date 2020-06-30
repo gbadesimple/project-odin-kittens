ae22173df2e2761c450b1675a5a4b53e34b81df2
gbadesimple <olugbadeolalekan@yahoo.com>
2020-06-30 21:45:26 +0100

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :kitten
 
-  root to: "kitten#index"
+  root to: "kittens#index"
 end
