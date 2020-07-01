60a9cf689baa81d3a88733699d06aa8dea37ce87
gbadesimple <olugbadeolalekan@yahoo.com>
2020-07-01 19:54:17 +0100

    kitten creation and all controller actions implemented with few gems

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,13 +1,10 @@
 class KittensController < ApplicationController
+
   def index
-    @kittens = Kitten.all
+    @kittens = Kitten.all.order(:created_at => "DESC")
   end
 
   def show
-    @kitten = Kitten.find(params[:id])
-  end
-
-  def edit
     @kitten = Kitten.find(params[:id])
   end
 
@@ -15,21 +12,58 @@ class KittensController < ApplicationController
     @kitten = Kitten.new
   end
 
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
   def create
     @kitten = Kitten.create(kitten_params)
+
+    @kitten.name.capitalize!
+    @kitten.cuteness.capitalize!
+    @kitten.softness.capitalize!
+
+    if @kitten.save
+      flash[:notice] = "Kitten successfully created"
+      redirect_to @kitten
+    else
+      flash[:alert] = "Something went wrong, please check your input"
+      render :new
+    end
   end
 
   def update
-    @kitten = Kitten.update(kitten_params)
+    @kitten = Kitten.find(params[:id])
+    @kitten.update(kitten_params)
+    
+    if @kitten.save
+      flash[:notice] = "Kitten successfully updated"
+      redirect_to kitten_path
+    else
+      flash[:alert] = "Something went wrong, please check your input"
+      render :edit
+    end
   end
+
 
   def destroy
     @kitten = Kitten.find(params[:id])
-    @kitten.destroy
+
+    if @kitten.destroy
+      flash[:notice] = "'#{@kitten.name}' successfully deleted."
+      redirect_to @kitten
+    else
+      flash[:alert] = "Unable to delete '#{@kitten.name}'"
+      render :show
+    end
   end
+
+
+
 
   private
   def kitten_params
     params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
+
 end
--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,6 @@
 class Kitten < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :age
+  validates_presence_of :cuteness
+  validates_presence_of :softness
 end
